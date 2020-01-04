dd0b9af8298b81a60741a5e1c946366fb061f4a6
nikolamand <nikolamand@gmail.com>
2020-01-04 13:33:29 +0100

    Fix Hover Fixed hover to allow changes to elements with !important attribute Save background and savebackgroundcolor will need to be replaced with colors from the localstorage or backend when it is finished

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 (function(global) {
-  const changeColor = () => {
-    var selectedColor = colorfyColorPicker.value;
-    //Remove images from background
-    selectedElement.style.background = "none";
-    selectedElement.style.backgroundColor = selectedColor;
-  };
-
   const init = () => {
     var selectedElement = null;
     //Saves background color for the element if there is inline CSS on the element
     var saveBackground = null;
+    var saveBackgroundColor = null;
     var colorfyColorPicker = null;
 
     addColorPicker();
     addListeners();
+  };
+  
+  const changeColor = () => {
+    var selectedColor = colorfyColorPicker.value;
+    //Remove images from background
+    selectedElement.style.setProperty("background", "none", "important");
+    selectedElement.style.setProperty("background-color", selectedColor, "important"); 
   };
 
   const addColorPicker = e => {
@@ -28,7 +29,7 @@
     colorfyColorPicker.hidden = true;
     document.getElementsByTagName("body")[0].appendChild(colorfyColorPicker);
 
-    //HTML5 color picker .oninput event seems to have a bugg that makes it act the same as .onchange event
+    //FIX HTML5 color picker .oninput event seems to have a bugg that makes it act the same as .onchange event
     //so this will need to be addressed in to be able to add a color selected by default (colorfyColorPicker.value)
     colorfyColorPicker.oninput = changeColor;
   };
@@ -37,15 +38,26 @@
   const hoverElements = e => {
     e = e || window.event;
     let target = e.target || e.srcElement;
-    saveBackground = target.style.backgroundColor;
-    target.style.backgroundColor = "rgba(0,130,255,0.3)";
+    
+    //FIX saveBackground and saveBackgroundColor, take the color from localstorage when it is created.
+    saveBackground = target.style.background;
+    saveBackgroundColor = target.style.backgroundColor;
+    target.style.setProperty("background", "none", "important");
+    target.style.setProperty("background-color", "rgba(0,40,80,0.3)", "important");
   };
 
   //Return  element background
   const resetHover = e => {
     e = e || window.event;
     let target = e.target || e.srcElement;
-    target.style.backgroundColor = saveBackground;
+    if(saveBackgroundColor)
+      target.style.setProperty("background-color", saveBackgroundColor);
+    else if(saveBackground)
+      target.style.setProperty("background", saveBackground);
+    else{
+      target.style.setProperty("background-color", "");
+      target.style.setProperty("background", "");
+    }
   };
 
   const removeListeners = () => {
@@ -66,7 +78,6 @@
     if (target == colorfyColorPicker) return;
     else selectedElement = target;
     elementInfo(selectedElement);
-    console.log(colorfyColorPicker);
     colorfyColorPicker.click();
     removeListeners();
     resetHover(e);
